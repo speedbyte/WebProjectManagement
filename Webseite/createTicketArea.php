0224c83b058583d39e3fb1da3a1d5e98573b465c
Dominik Heinrich <dominik.heinrich@student.uni-tuebingen.de>
2016-08-29 22:48:19 +0200

    createTicketArea geandert

--- Webseite/createTicketArea.php
+++ Webseite/createTicketArea.php
@@ -10,13 +10,19 @@
 <body>
 	<content>
 	
-<p> <b>Formulieren Sie ihr Problem </b></p>
+<p> <b>Bitte formulieren Sie ihr Problem </b></p>
 	
 	
 	
 	<form action="createTicket.php" method="GET">	
+		<label for="Vorname">Vorname: </label>
+		<input type="text" name="vorname"><br>
+		<label for="Nachname">Nachname: </label>
+		<input type="text" name="nachname"><br>
+
 		<label for="username">Username: </label>
-		<input type="text" name="username"><br>
+		<input type="text" name="nachname" value="optional"><br><br>
+
 		<label for="problem">Problem: </label><br>
 		<textarea cols="35" rows="4" name="problem">
 		Bitte schildern Sie ihr Problem!
